00c74dd3ec0c8100916cba5989117245a315cea9
Xun Yi <xunyi.work@gmail.com>
2023-06-14 20:36:50 +0800

    Added in __binary_encode and __scale_numerical

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from category_encoders import BinaryEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 from typing import Optional
 class DataPreprocessor:
     """
@@ -10,6 +12,7 @@ class DataPreprocessor:
         Assume that the table has been fully joined.
         """
         self.df = df
+        self.scaler = MinMaxScaler()
         # self._validate_df()
 
     def __validate_df(self):
@@ -18,7 +21,7 @@ class DataPreprocessor:
         """
         return True
     
-    def __map_categorical(self, col: pd.Series, custom_mapping:Optional[dict]=None):
+    def __map_categorical(self, col: pd.Series, custom_mapping:Optional[dict]=None)->pd.Series:
         """
         Map common values to numerical values. Provide custom mapping if required.
         """
@@ -31,6 +34,22 @@ class DataPreprocessor:
         if custom_mapping:
             return col.map(custom_mapping)
         return col.map(mapping)
+    
+    def __binary_encode(self, col:pd.Series)->pd.Series:
+        """
+        Returns a pd.Df that has to be concatted to the main df using the following:
+        output_df = pd.concat([output_df, return_value], axis=1)
+        """
+        binary_encoder = BinaryEncoder(cols=['churn_category'])
+        binary_encoder.fit_transform(col)
+        churn_cat = binary_encoder.transform(col)
+        return churn_cat
+    
+    def __scale_numerical(self,col:pd.Series)->pd.Series:
+        """
+        Scales numerical columns using the same scaler object associated with this class.
+        """
+        return self.scaler.fit_transform(col)
         
     
 if __name__ == "__main__":
