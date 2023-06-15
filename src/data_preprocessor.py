edc7112b853f70f57717b44f3f17411ab627e4a1
Xun Yi <xunyi.work@gmail.com>
2023-06-16 00:12:42 +0800

    Fixed one hot encoded and scaling

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -2,6 +2,9 @@ import pandas as pd
 from category_encoders import BinaryEncoder
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 from typing import Optional
+
+from data_etl import DataETL
+
 class DataPreprocessor:
     """
     Perform data cleaning steps on dataset.
@@ -15,6 +18,7 @@ class DataPreprocessor:
         print(self.df.keys())
         self.scaler = MinMaxScaler()
         self.one_hot_encoder = OneHotEncoder()
+        self.label_encoder = LabelEncoder()
         # self._validate_df()
         self.__preprocess()
 
@@ -28,6 +32,7 @@ class DataPreprocessor:
         """
         Perform all preprocessing steps.
         """
+        # Map string values to categorical
         self.df['has_internet_service'] = self.__map_categorical(self.df['has_internet_service'])
         self.df['has_phone_service'] = self.__map_categorical(self.df['has_phone_service'])
         self.df['has_unlimited_data'] = self.__map_categorical(self.df['has_unlimited_data'])
@@ -43,7 +48,41 @@ class DataPreprocessor:
         self.df['senior_citizen'] = self.__map_categorical(self.df['senior_citizen'])
         self.df['married'] = self.__map_categorical(self.df['married'])
         self.df['gender'] = self.__map_categorical(self.df['gender'])
-        
+
+        # Scaling numerical values
+        self.df['num_referrals'] = self.__scale_numerical(self.df['num_referrals'])
+        self.df['age'] = self.__scale_numerical(self.df['age'])
+        self.df['tenure_months'] = self.__scale_numerical(self.df['tenure_months'])
+        self.df['avg_long_distance_fee_monthly'] = self.__scale_numerical(self.df['avg_long_distance_fee_monthly'])
+        self.df['total_long_distance_fee'] = self.__scale_numerical(self.df['total_long_distance_fee'])
+        self.df['avg_gb_download_monthly'] = self.__scale_numerical(self.df['avg_gb_download_monthly'])
+        self.df['total_monthly_fee'] = self.__scale_numerical(self.df['total_monthly_fee'])
+        self.df['total_charges_quarter'] = self.__scale_numerical(self.df['total_charges_quarter'])
+        self.df['total_refunds'] = self.__scale_numerical(self.df['total_refunds'])
+        self.df['population'] = self.__scale_numerical(self.df['population'])
+
+        # Label encode 
+        self.df['status'] = self.__label_encode(self.df['status'])
+
+        # Binary encode
+        churn_df = self.__binary_encode(self.df['churn_category'])
+        assert not churn_df.empty, "Churn DF is empty!"
+        self.df = pd.concat([self.df, churn_df], axis=1)
+        self.df.drop('churn_category', axis=1, inplace=True)
+
+        # One hot encode
+        print("Trying to one hot encode")
+        print(f"Input is {self.df['payment_method']} ")
+        _ = self.__one_hot_encode(self.df['payment_method'])
+        print(f"Output is {_} ")
+
+
+
+
+
+    def __label_encode(self, col:pd.Series)->pd.Series:
+        return self.label_encoder.fit_transform(col)        
+
     
     def __map_categorical(self, col: pd.Series, custom_mapping:Optional[dict]=None)->pd.Series:
         """
@@ -73,20 +112,29 @@ class DataPreprocessor:
         """
         Scales numerical columns using the same scaler object associated with this class.
         """
-        return self.scaler.fit_transform(col)
+        scaled_col = self.scaler.fit_transform(col.values.reshape(-1, 1))
+        scaled_df = pd.DataFrame(scaled_col, columns=[col.name])
+        return scaled_df
     
-    def __one_hot_encode(self, col:pd.Series)->pd.Series:
+    def __one_hot_encode(self, col: pd.Series) -> pd.DataFrame:
         """
-        One hot encodes categorical columns using the same encoder object associated with this class.
+        One hot encodes a categorical column using the same encoder object associated with this class.
+        Returns the result as a DataFrame.
         """
-        return self.one_hot_encoder.fit_transform(col)
+        oh_encoded = pd.get_dummies(col)
+        return oh_encoded
+
         
     
 if __name__ == "__main__":
-    ACC_PATH = "research/data_given/1_account.csv"
-    ACC_DF = pd.read_csv(ACC_PATH)
-    # print(ACC_DF.head())
-    dp = DataPreprocessor(ACC_DF)
-    dp.head()
+    # Building ETL Object
+    etl = DataETL()
+    etl.use_local_data("research/data_given/")
+    etl.join_tables()
+
+    df = etl.get_df()
+
+    dp = DataPreprocessor(df)
+    dp.df.head()
  
 
