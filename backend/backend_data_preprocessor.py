8c02ebf24151fc55822ad7f5307ea9b63162d07f
Xun Yi <xunyi.work@gmail.com>
2023-06-27 21:38:35 +0800

    Added in preprocessing step for catboost

--- backend/backend_data_preprocessor.py
+++ backend/backend_data_preprocessor.py
@@ -52,60 +52,71 @@ class DataPreprocessor:
         """
         return True
     
-    def preprocess_input(self, df:pd.DataFrame)->pd.DataFrame:
+    def preprocess_input(self, df:pd.DataFrame, model:str)->pd.DataFrame:
         """
         Perform all preprocessing steps.
         """
-        # Map string values to categorical
-        self.df = df
-        self.df['has_internet_service'] = self.__map_categorical(self.df['has_internet_service'], on_input=True)
-        self.df['has_phone_service'] = self.__map_categorical(self.df['has_phone_service'], on_input=True)
-        self.df['has_unlimited_data'] = self.__map_categorical(self.df['has_unlimited_data'], on_input=True)
-        self.df['has_multiple_lines'] = self.__map_categorical(self.df['has_multiple_lines'], on_input=True)
-        self.df['has_premium_tech_support'] = self.__map_categorical(self.df['has_premium_tech_support'], on_input=True)
-        self.df['has_online_security'] = self.__map_categorical(self.df['has_online_security'], on_input=True)
-        self.df['has_online_backup'] = self.__map_categorical(self.df['has_online_backup'], on_input=True)
-        self.df['has_device_protection'] = self.__map_categorical(self.df['has_device_protection'], on_input=True)
-        self.df['paperless_billing'] = self.__map_categorical(self.df['paperless_billing'], on_input=True)
-        self.df['stream_movie'] = self.__map_categorical(self.df['stream_movie'], on_input=True)
-        self.df['stream_music'] = self.__map_categorical(self.df['stream_music'], on_input=True)
-        self.df['stream_tv'] = self.__map_categorical(self.df['stream_tv'], on_input=True)
-        self.df['senior_citizen'] = self.__map_categorical(self.df['senior_citizen'], on_input=True)
-        self.df['married'] = self.__map_categorical(self.df['married'], on_input=True)
-        self.df['gender'] = self.__map_categorical(self.df['gender'], on_input=True)
-        self.df['contract_type'] = self.__map_categorical(self.df['contract_type'], custom_mapping={'Month-to-Month':0, 'One Year':1, 'Two Year':2})
+        if model == "catboost":
+            self.df = df
+            self.df['contract_type'] = self.__map_categorical(self.df['contract_type'], custom_mapping={'Month-to-Month':0, 'One Year':1, 'Two Year':2})
+            self.df['num_referrals'] = self.__scale_numerical(self.df['num_referrals'], on_input=True)
+            self.df['total_long_distance_fee'] = self.__scale_numerical(self.df['total_long_distance_fee'], on_input=True)
+            self.df['tenure_months'] = self.__scale_numerical(self.df['tenure_months'], on_input=True)
+            self.df['total_charges_quarter'] = self.__scale_numerical(self.df['total_charges_quarter'], on_input=True)
+            return self.df
+        elif model == "xgboost":
 
-        # Scaling numerical values
-        self.df['num_referrals'] = self.__scale_numerical(self.df['num_referrals'], on_input=True)
-        self.df['age'] = self.__scale_numerical(self.df['age'], on_input=True)
-        self.df['tenure_months'] = self.__scale_numerical(self.df['tenure_months'], on_input=True)
-        self.df['avg_long_distance_fee_monthly'] = self.__scale_numerical(self.df['avg_long_distance_fee_monthly'], on_input=True)
-        self.df['total_long_distance_fee'] = self.__scale_numerical(self.df['total_long_distance_fee'], on_input=True)
-        self.df['avg_gb_download_monthly'] = self.__scale_numerical(self.df['avg_gb_download_monthly'], on_input=True)
-        self.df['total_monthly_fee'] = self.__scale_numerical(self.df['total_monthly_fee'], on_input=True)
-        self.df['total_charges_quarter'] = self.__scale_numerical(self.df['total_charges_quarter'], on_input=True)
-        self.df['total_refunds'] = self.__scale_numerical(self.df['total_refunds'], on_input=True)
-        self.df['population'] = self.__scale_numerical(self.df['population'], on_input=True)
+            # Map string values to categorical
+            self.df = df
+            assert "has_internet_service" in self.df.columns, "has_internet_service column not found"
+            self.df['has_internet_service'] = self.__map_categorical(self.df['has_internet_service'], on_input=True)
+            self.df['has_phone_service'] = self.__map_categorical(self.df['has_phone_service'], on_input=True)
+            self.df['has_unlimited_data'] = self.__map_categorical(self.df['has_unlimited_data'], on_input=True)
+            self.df['has_multiple_lines'] = self.__map_categorical(self.df['has_multiple_lines'], on_input=True)
+            self.df['has_premium_tech_support'] = self.__map_categorical(self.df['has_premium_tech_support'], on_input=True)
+            self.df['has_online_security'] = self.__map_categorical(self.df['has_online_security'], on_input=True)
+            self.df['has_online_backup'] = self.__map_categorical(self.df['has_online_backup'], on_input=True)
+            self.df['has_device_protection'] = self.__map_categorical(self.df['has_device_protection'], on_input=True)
+            self.df['paperless_billing'] = self.__map_categorical(self.df['paperless_billing'], on_input=True)
+            self.df['stream_movie'] = self.__map_categorical(self.df['stream_movie'], on_input=True)
+            self.df['stream_music'] = self.__map_categorical(self.df['stream_music'], on_input=True)
+            self.df['stream_tv'] = self.__map_categorical(self.df['stream_tv'], on_input=True)
+            self.df['senior_citizen'] = self.__map_categorical(self.df['senior_citizen'], on_input=True)
+            self.df['married'] = self.__map_categorical(self.df['married'], on_input=True)
+            self.df['gender'] = self.__map_categorical(self.df['gender'], on_input=True)
+            self.df['contract_type'] = self.__map_categorical(self.df['contract_type'], custom_mapping={'Month-to-Month':0, 'One Year':1, 'Two Year':2})
 
-        # One hot encode
-        # Payment method
-        ohe_payment_method = self.__one_hot_encode(self.df['payment_method'], self.payment_method_ohe, on_input=True)
-        for col_name in ohe_payment_method.columns:
-            assert col_name not in self.df.columns, f"Column '{col_name}' already exists in the DataFrame."
-        self.df = pd.concat([self.df, ohe_payment_method], axis=1)
-        # Dropping payment method
-        self.df = self.df.drop('payment_method', axis=1)
-        # Internet_type
-        ohe_internet_type = self.__one_hot_encode(self.df['internet_type'], self.internet_type_ohe, on_input=True)
-        for col_name in ohe_internet_type.columns:
-            assert col_name not in self.df.columns, f"Column '{col_name}' already exists in the DataFrame."
+            # Scaling numerical values
+            self.df['num_referrals'] = self.__scale_numerical(self.df['num_referrals'], on_input=True)
+            self.df['age'] = self.__scale_numerical(self.df['age'], on_input=True)
+            self.df['tenure_months'] = self.__scale_numerical(self.df['tenure_months'], on_input=True)
+            self.df['avg_long_distance_fee_monthly'] = self.__scale_numerical(self.df['avg_long_distance_fee_monthly'], on_input=True)
+            self.df['total_long_distance_fee'] = self.__scale_numerical(self.df['total_long_distance_fee'], on_input=True)
+            self.df['avg_gb_download_monthly'] = self.__scale_numerical(self.df['avg_gb_download_monthly'], on_input=True)
+            self.df['total_monthly_fee'] = self.__scale_numerical(self.df['total_monthly_fee'], on_input=True)
+            self.df['total_charges_quarter'] = self.__scale_numerical(self.df['total_charges_quarter'], on_input=True)
+            self.df['total_refunds'] = self.__scale_numerical(self.df['total_refunds'], on_input=True)
+            self.df['population'] = self.__scale_numerical(self.df['population'], on_input=True)
 
-        self.df = pd.concat([self.df, ohe_internet_type], axis=1)
-        # Dropping internet type
-        self.df = self.df.drop('internet_type', axis=1)
-        self.df.columns = self.df.columns.astype(str)
+            # One hot encode
+            # Payment method
+            ohe_payment_method = self.__one_hot_encode(self.df['payment_method'], self.payment_method_ohe, on_input=True)
+            for col_name in ohe_payment_method.columns:
+                assert col_name not in self.df.columns, f"Column '{col_name}' already exists in the DataFrame."
+            self.df = pd.concat([self.df, ohe_payment_method], axis=1)
+            # Dropping payment method
+            self.df = self.df.drop('payment_method', axis=1)
+            # Internet_type
+            ohe_internet_type = self.__one_hot_encode(self.df['internet_type'], self.internet_type_ohe, on_input=True)
+            for col_name in ohe_internet_type.columns:
+                assert col_name not in self.df.columns, f"Column '{col_name}' already exists in the DataFrame."
 
-        return self.df
+            self.df = pd.concat([self.df, ohe_internet_type], axis=1)
+            # Dropping internet type
+            self.df = self.df.drop('internet_type', axis=1)
+            self.df.columns = self.df.columns.astype(str)
+
+            return self.df
 
     
     def __label_encode(self, col:pd.Series, on_input:bool=False)->pd.Series:
