10e4da9f5fdc2978dd0187d2362e6e518ee70dc7
Xun Yi <xunyi.work@gmail.com>
2023-07-01 11:17:38 +0800

    Addded in vue

---
+++ frontend/churn-prediction-ai300/src/components/Form.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="row vh-100 align-items-center justify-content-center">
+    <div class="col-8 bg-light h-75 rounded">
+      <div class="row h-100 p-3">
+        <div class="col-5 bg-dark rounded text-white ">
+          Random Content here
+        </div>
+        <div class="col-7 bg-secondary rounded text-white ps-5">
+          Form here
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
