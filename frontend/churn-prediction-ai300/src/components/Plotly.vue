93e7fb4341a5a58b28a20b965eea12e8c7704f98
Xun Yi <xunyi.work@gmail.com>
2023-07-08 13:48:24 +0800

    Updated plot

--- frontend/churn-prediction-ai300/src/components/Plotly.vue
+++ frontend/churn-prediction-ai300/src/components/Plotly.vue
@@ -1,20 +1,39 @@
 <script setup>
 import Plotly from 'plotly.js-dist';
 import { ref, onMounted } from 'vue';
+import metrics_across_thresholds from '../assets/metrics_across_thresholds.json';
 
-const TESTER = ref("tester");
-console.log(TESTER.value);
-
+const plot = ref("plot");
 onMounted(() => {
-  Plotly.newPlot(TESTER.value, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16]
-  }], {
-    margin: { t: 0 }
+  const keys = Object.keys(metrics_across_thresholds);
+  const accuracyValues = Object.values(metrics_across_thresholds).map(item => item.accuracy);
+  const tooltips = keys.map(key => {
+    const metrics = metrics_across_thresholds[key];
+    return `Key: ${key}<br>Accuracy: ${metrics.accuracy}<br>Precision: ${metrics.precision}<br>Recall: ${metrics.recall}<br>F1: ${metrics.f1}<br>Factor: ${metrics.factor}`;
   });
+  // Plotting
+  const data = {
+    x: keys,
+    y: accuracyValues,
+    hovertemplate: tooltips.map(tooltip => `<b>${tooltip}</b><extra></extra>`),
+    type: 'scatter',
+    mode: 'lines+markers',
+  };
+
+  const layout = {
+    xaxis: {
+      title: 'Key'
+    },
+    yaxis: {
+      title: 'Accuracy'
+    },
+    hovermode: 'closest',
+  };
+
+  Plotly.newPlot('plot', [data], layout);
 });
 </script>
 
 <template>
-  <div id="tester" :ref="TESTER" style="width: 600px; height: 250px;"> Hello </div>
+  <div id="plot" :ref="plot" style="width: 1000px; height: 500px;"> Hello </div>
 </template>
